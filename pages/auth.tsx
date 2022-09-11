24ef024c0d97e10191d388f616f770fa57c4458d
Kris <kristian120304@gmail.com>
2022-09-10 20:38:38 -0400

    Kris changes

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,4 @@
+import style from "./auth.module.scss"
 import Button from "../components/Button/Button";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -15,11 +16,12 @@ const app = initializeApp(firebaseConfig);
 
 const auth=()=>{
    return (
-    <div>
+    <div className={[style.card].join(" ")}>
         <h1>focus Buddy</h1>
         <form>
             <label htmlFor="email">email:</label>
             <input id="email" name="email"></input>
+            <br/>
             <label htmlFor="password">password:</label>
             <input id="password"></input>
             <div onClick={signUp}><Button text="Create Account" type="dark"></Button></div>
