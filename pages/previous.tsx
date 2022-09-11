8aa071375afbe4122c89322f5f8ee8c0b3014ecb
Leon Kipkoech <leonkipkip@gmail.com>
2022-09-10 22:53:00 -0400

    front end started

--- pages/previous.tsx
+++ pages/previous.tsx
@@ -2,7 +2,7 @@ import ChartComponent from "../components/chart/Chart";
 const previous = () =>{
 return (
     <div>
-    <ChartComponent name={'promodoro 1 session 1'} points={[10.0, 20.2, 39.1, 21.4, 19.9, 20.2, 39.8, 18.6, 49.6]}></ChartComponent>
+          <ChartComponent name={'promodoro 1 session 1'} points={[10.0, 20.2, 39.1, 21.4, 19.9, 20.2, 39.8, 18.6, 49.6]}></ChartComponent>
     </div>
 );
 }
